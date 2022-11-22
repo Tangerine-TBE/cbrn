c7c4c75adf2eb1778bab8565f67b8d1b8823d344
TanBE <947624811@qq.com>
2022-11-22 11:24:02 +0800

    项目构建

---
+++ app/src/main/java/com/dangkang/cbrn/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.dangkang.cbrn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.inputmethod.InputBinding
+import com.dangkang.cbrn.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
