1c4a441e2c4396def2389c80b311b1dd1e16cefe
tangerine <947624811@qq.com>
2023-02-17 16:53:13 +0800

    阻塞式写入的蓝牙

--- app/src/main/java/com/dangkang/cbrn/service/AutoRequestServer.kt
+++ app/src/main/java/com/dangkang/cbrn/service/AutoRequestServer.kt
@@ -18,7 +18,7 @@ class AutoRequestServer : Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         /*1.try to connect the index 0f 0 device */
-        Thread{
+        Thread {
             val deviceInfo = DaoTool.queryAllDeviceInfo()
             val index = 0
             dfs(deviceInfo, index)
@@ -26,51 +26,33 @@ class AutoRequestServer : Service() {
 
         return START_NOT_STICKY
     }
-
     private fun dfs(deviceInfo: List<DeviceInfo>, index: Int) {
-
-        BleManager.getInstance().connect(deviceInfo[0].bleDevice, object : BleGattCallback() {
-            override fun onStartConnect() {
-            }
-
-            override fun onConnectFail(bleDevice: BleDevice?, exception: BleException?) {
-            }
-
-            override fun onConnectSuccess(
-                bleDevice: BleDevice?, gatt: BluetoothGatt?, status: Int
-            ) {
+        BleManager.getInstance().connect(deviceInfo[index].bleDevice, object : BleGattCallback() {
+            override fun onStartConnect() {}
+            override fun onConnectFail(bleDevice: BleDevice?, exception: BleException?) {}
+            override fun onConnectSuccess(bleDevice: BleDevice?, gatt: BluetoothGatt?, status: Int) {
                 val serUUID = gatt?.services?.get(2)?.uuid.toString()//可用Service特征
                 val wriUUID =
                     gatt?.services?.get(2)?.characteristics?.get(1)?.uuid.toString()//可用写入特征
                 val resultCmd = byteArrayOf(0x23)
-                BleManager.getInstance()
-                    .write(bleDevice, serUUID, wriUUID, resultCmd, object : BleWriteCallback() {
-                        override fun onWriteSuccess(
-                            current: Int, total: Int, justWrite: ByteArray?
-                        ) {
+                BleManager.getInstance().write(bleDevice, serUUID, wriUUID, resultCmd, object : BleWriteCallback() {
+                        override fun onWriteSuccess(current: Int, total: Int, justWrite: ByteArray?) {
                             L.e("写入ok")
-                            BleManager.getInstance().disconnectAllDevice()
+                            BleManager.getInstance().disconnect(bleDevice)
                             val next = index + 1
-                            if (next == deviceInfo.size){
+                            if (next == deviceInfo.size) {
                                 stopSelf()
                                 return
                             }
                             dfs(deviceInfo, next)
                         }
-
                         override fun onWriteFailure(exception: BleException?) {
                             L.e(exception.toString())
                         }
                     })
             }
-
-            override fun onDisConnected(
-                isActiveDisConnected: Boolean, device: BleDevice?, gatt: BluetoothGatt?, status: Int
-            ) {
-            }
+            override fun onDisConnected(isActiveDisConnected: Boolean, device: BleDevice?, gatt: BluetoothGatt?, status: Int) {}
         })
-
-
     }
 
 
